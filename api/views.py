ce734f3da8cedc0bb11e7d5a018a81e2e3a5831f
Bambo <kyrstke@gmail.com>
2024-06-04 20:16:45 +0200

    Docker

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from api.models import Circuit
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'index2.html', context={})
+
+
+def circuits(request):
+    return render(request, 'index2.html', {'circuits': Circuit.objects.all()})
